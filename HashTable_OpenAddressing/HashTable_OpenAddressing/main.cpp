3281ce46bd8aa8b37e1621b30d1d5f35b55c5c78
Zachary <zackbarry13@gmail.com>
2018-10-26 11:24:57 -0700

    Enlarged data set

--- HashTable_OpenAddressing/HashTable_OpenAddressing/main.cpp
+++ HashTable_OpenAddressing/HashTable_OpenAddressing/main.cpp
@@ -7,7 +7,19 @@ int main()
 {
 	HashTable myhash;
 	myhash.HashInsert("Zack", 22);
+	myhash.HashInsert("Matt", 19);
+	myhash.HashInsert("Glen", 22);
+	myhash.HashInsert("Trevor", 22);
+	myhash.HashInsert("Lindsey", 23);
+	myhash.HashInsert("Liz", 22);
+	myhash.HashInsert("Grandpa", 81);
+	myhash.HashInsert("Queen Elizabeth", 86);
+	myhash.HashInsert("Personable A", 24);
+	myhash.HashInsert("Personable B", 66);
+	myhash.HashInsert("Born Jan1 2017", 2);
+
 	myhash.PrintTable();
+
 	std::cin.get();
 	return 0;
 }
