98be98bfca62a395fb71b450e58476cfd492e89c
Zachary <zackbarry13@gmail.com>
2018-10-29 17:27:22 -0700

    uncommented private section of Tree so that the root variable is private. Made transplant private as it is a member subroutine.

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.h
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.h
@@ -19,13 +19,13 @@ class Tree
 {
 public:
 	Tree();
-	Tree(Element*);
-//private:
+	Tree(Element*);	// set root to be equal to the argument
+private:
 	Element* root;
 public:
 	void print_inorder(Element*);	// if printing the subtree with argument as its base
 	void print_inorder();			// if printing the whole tree
-	Element* search(int);	// return pointer to Element holding the argument as its key
+	Element* search(int);		// return pointer to Element holding the argument as its key
 	Element* minimum(Element*);	// if searching a subtree (i.e. starting at a specific Element)
 	Element* minimum();			// if searching the whole tree (i.e. starting at root)
 	Element* maximum(Element*);
